0cefe7a869ba759cffa38bcd39013fb765bf5cb5
Dharmesh Kamdar <dkamdar5@users.noreply.github.com>
2020-07-24 13:28:37 -0500

    accessing flask api in react

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,23 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [places, setPlaces] = useState();
+
+  useEffect(() => {
+    fetch('/api/places').then(res => res.json()).then(data => {
+      setPlaces(JSON.parse(data))
+      console.log(data)
+    })
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ul>
+          { places && 
+            places.map((v, i) => {
+              return (
+                <>
+                  <li key={i}>Name: {v["name"]}</li>
+                  <li key={i}>Location: {v["location"]}</li>
+                </>
+              )
+            })
+          }
+        </ul>
       </header>
     </div>
   );
