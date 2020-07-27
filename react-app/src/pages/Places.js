6fe8bdb1fac8a4e53fc4cbf48cdb86b99005cb20
Dharmesh Kamdar <dkamdar5@users.noreply.github.com>
2020-07-26 19:03:24 -0500

    adding link in places page to individual place

--- react-app/src/pages/Places.js
+++ react-app/src/pages/Places.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
 const Places = () => {
     const [places, setPlaces] = useState();
@@ -17,6 +18,7 @@ const Places = () => {
               places.map((v, i) => {
                 return (
                   <>
+                    <Link to={`/places/${v["_id"]["$oid"]}`}>Go to {v["name"]}</Link>
                     <li key={i}>Name: {v["name"]}</li>
                     <li key={i}>Location: {v["location"]}</li>
                   </>
